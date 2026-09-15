e90a9364786873558f2dc1c69da79c38891f95fd
Chadrick <ichadrickryan@gmail.com>
2026-09-15 20:40:34 +0800

    Updateunit converter

--- 01-python-foundations/02-unit-converter/src/unit_converter.py
+++ 01-python-foundations/02-unit-converter/src/unit_converter.py
@@ -4,49 +4,49 @@
 def findUnit(item, unit):
     match unit:
         case 1: # cm - in
-            converted = item * 0.0039370079
+            converted = item * 0.39370079
             print("\n------------------------------------")
             print("\tcentimeter to inches")
             print("------------------------------------")
             return f"{item}cm --> {converted:.4f}in\n"
         case 2: # mm - in
-            converted = item * 0.0003937008
+            converted = item * 0.03937008
             print("\n------------------------------------")
             print("\tmilimeter to inches")
             print("------------------------------------")
             return f"{item}mm --> {converted:.4f}in\n"
         case 3: # meter - feet
-            converted = item * 0.032808399 
+            converted = item * 3.2808399
             print("\n------------------------------------")
             print("\tmeter to feet")
             print("------------------------------------")
             return f"{item}m --> {converted:.4f}ft\n"
         case 4: # km - miles
-            converted = item * 0.0062137119 
+            converted = item * 0.62137119 
             print("\n------------------------------------")
             print("\tkilometer to miles")
             print("------------------------------------")
             return f"{item}km --> {converted:.4f}mi\n"
         case 5: # cm - feet
-            converted = item * 0.000328084 
+            converted = item * 0.0328084 
             print("\n------------------------------------")
             print("\tcentimeter to feet")
             print("------------------------------------")
             return f"{item}cm --> {converted:.4f}ft\n"
         case 6: # in - feet
-            converted = item * 0.0008333333 
+            converted = item * 0.08333333 
             print("\n------------------------------------")
             print("\tinches to feet")
             print("------------------------------------")
             return f"{item}in --> {converted:.4f}ft\n"
         case 7: # meter - yard
-            converted = item * 0.010936133 
+            converted = item * 1.0936133 
             print("\n------------------------------------")
             print("\tmeter to yard")
             print("------------------------------------")
             return f"{item}m --> {converted:.4f}yd\n"
         case 8: # cm - km
-            converted = item * 0.0000001
+            converted = item * 0.00001
             print("\n------------------------------------")
             print("\tcemtimeter to kilometer")
             print("------------------------------------")
@@ -88,7 +88,7 @@ while True:
     if choice == 'n':
         break
     elif choice == 'y':
-        pass
+        continue
     else:
         print("Your Choice not found")
         
